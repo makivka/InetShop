83a1cf2a082b1f4d641e378d3386bcb1cf4eaabb
makivka79@gmail.com <makivka79@gmail.com>
2022-09-18 19:02:36 +0300

    Add project files.

---
+++ Cart.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InetShop
+{
+    class Cart
+    {
+        List<Goods> GoodsCart { get; set; }
+        public void AddToCart(Goods good)
+        {
+             GoodsCart.Add(good);
+        }
+        public void RemoveFromCart(Goods good)
+        {
+            GoodsCart.Remove(good);
+        }
+        public void View()
+        {
+            foreach ( var good in GoodsCart)
+            {
+                string printGood = good.View();
+                Console.WriteLine(printGood);
+            }
+        }
+    }
+}
---
+++ Goods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InetShop
+{
+    class Goods
+    {public string Name { get; set; }
+        public Unit Units { get; set; }
+        public float Price { get; set; }
+        
+        public string View()
+        {
+            string view = Name + "   " + Convert.ToString(Units) + "   "+ Convert.ToString(Price);
+            return view;
+        }
+        public enum Unit 
+        {
+            piece,
+            m,
+            kg
+        }
+    }
+}
